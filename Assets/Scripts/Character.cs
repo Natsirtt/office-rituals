2f6a38bff23c617ab7c9555ae2427afa5f8a1386
Eric tuvesson <Eric tuvesson>
2016-01-29 22:50:09 +0100

    Hey :bowtie:

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,22 +2,53 @@
 using System.Collections;
 
 public abstract class Meter {
+    public float Value;
 
+    void Start() {
+        Value = 0.0f;
+    }
+
+    public abstract void CalcWork(ref float value);
 }
+
+
 
 public class Character : MonoBehaviour {
 
-    public float WorkMeter;
-    public float CoffeeMeter;
-
+    private float WorkMeter;
+    private float CoffeeMeter;
+    private Location previousLocation;
 
 	// Use this for initialization
-	void Start () {
-	    
+	void Start() {
+        this.WorkMeter = 0.0f;
+        this.CoffeeMeter = 50.0f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 	    
 	}
+    
+    public void SetLocation(Location location) {
+
+    }
+
+    public void DoLocationAction() {
+
+    }
+
+    public void AddCoffee(float value) {
+        CoffeeMeter += value;
+        CoffeeMeter = Mathf.Clamp(0.0f, 100.0f);
+    }
+
+    public void AddWork(float value) {
+        WorkMeter += value;
+        WorkMeter = Mathf.Clamp(0.0f, 100.0f);
+
+        if (WorkMeter >= 100.0f) {
+            // Win?
+        }
+    }
 }
