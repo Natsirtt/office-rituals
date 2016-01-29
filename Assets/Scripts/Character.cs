f590ffcd8050b3428fcafc2fca12f22672077a13
Eric tuvesson <Eric tuvesson>
2016-01-29 22:22:49 +0100

    'hello'

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Meter {
+
+}
+
+public class Character : MonoBehaviour {
+
+    public float WorkMeter;
+    public float CoffeeMeter;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
