4ff249aee7640e36bbd3d7ae9cb721262311f7b5
Simon Hultgren <simon@hultech.se>
2016-01-29 22:50:45 +0100

    location classes

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Location : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+
+		//coll.gameObject.SetLocation (this);
+		
+	} 
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
