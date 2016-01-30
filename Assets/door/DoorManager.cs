c54b1f8304b0ed4d6f03825485f8c0ff20834352
Simon Hultgren <simon@hultech.se>
2016-01-30 13:29:25 +0100

    added Door

---
+++ Assets/door/DoorManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorManager : MonoBehaviour {
+
+	public Animator animController;
+
+
+	// Use this for initialization
+	void Start () {
+		//animController = GetComponentInChildren<Animator> ();
+	}
+	void OnTriggerEnter(Collider coll) 
+	{
+		animController.SetTrigger ("openDoor");
+	}
+	
+	void OnTriggerExit(Collider coll) 
+	{
+		animController.SetTrigger ("closeDoor");
+	} 
+}
