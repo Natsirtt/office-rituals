702aa39e01a49c20db92fbeb6cd69866f7fa345f
unknown <eric.tuvesson@gmail.com>
2016-01-30 22:06:11 +0100

    global work bar should work now

--- Assets/Scripts/GlobalWorkMeterGUI.cs
+++ Assets/Scripts/GlobalWorkMeterGUI.cs
@@ -1,11 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 using System.Collections;
 
 public class GlobalWorkMeterGUI : MonoBehaviour {
 
-	void onGUI() {
-		GUIDrawRect (new Rect (0, 0, 500, 20), Color.white);
+	void OnGUI() {
+
+		GUIDrawRect (new Rect (0, 0, Screen.width, 20), Color.white);
+
+		float x = 0;
+
+		var workInst = WorkMeterManager.GetInstance ();
+		for (int i = 0; i < workInst.AllWork.Keys.Count; i++) {
+
+			Color color = new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f));
+
+			Character key = (Character)workInst.AllWork.Keys.ElementAt(i);
+			float value = workInst.GetWork(key);
+
+			float width = value * Screen.width;
+			GUIDrawRect (new Rect (x, 0, width, 20), color);
+			x += width;
+		}
 	}
 
 	private static Texture2D _staticRectTexture;
