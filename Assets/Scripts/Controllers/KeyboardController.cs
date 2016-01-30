38040c272ba2c2dde59a1e41a9bf33d3c2eec78e
Tristan Louet <tristan.louet.dev@gmail.com>
2016-01-30 01:10:35 +0100

    Added Controller abstract class. Moved away movements from Character class to modulable Controller components. Implemented a basic KeyboardController.

---
+++ Assets/Scripts/Controllers/KeyboardController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class KeyboardController : Controller
+{
+    protected override bool IsActionPressed()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+
+    protected override Vector2 ComputeMovement()
+    {
+        Vector2 result = Vector2.zero;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow))
+        {
+            result.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            result.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            result.x += 1;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            result.x -= 1;
+        }
+        return result;
+    }
+}
