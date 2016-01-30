dcb34a199746576e6ad30cba81572209a804a3f3
Gustav <sir.gustav.the.coder@gmail.com>
2016-01-30 22:58:51 +0100

    soothing the moving script

--- Assets/Scripts/BasicCharacterAnimation.cs
+++ Assets/Scripts/BasicCharacterAnimation.cs
@@ -33,22 +33,28 @@ public class BasicCharacterAnimation : MonoBehaviour {
 		this.start_ = this.transform.localRotation;
 		this.current_facing_ = this.FacingQuat;
 	}
+
+	float weight_ = 0;
 	
 	// Update is called once per frame
 	void Update () {
 		this.current_facing_ = Quaternion.RotateTowards(this.current_facing_, this.FacingQuat, 10);
 		var base_face = this.start_ * this.current_facing_;
-		
+
+		float change = -1;
 		if( HasMoved ) {
-			this.wiggle_timer_ += Time.deltaTime * 0.75f;
-			while( this.wiggle_timer_ > 1.0f ) this.wiggle_timer_ -= 1.0f;
-			var smooth = Mathf.Sin(wiggle_timer_ * 2 * Mathf.PI);
-			var a = smooth * 10;
-			this.transform.localRotation = base_face * Quaternion.Euler(0, 0, a);
+			change = 1;
 		}
-		else {
-			this.wiggle_timer_ = 0;
-			this.transform.localRotation = base_face;
-		}
+		change *= 5 * Time.deltaTime;
+
+		this.weight_ = Mathf.Min(1, Mathf.Max(0, this.weight_ + change));
+
+		var weight = this.weight_;
+
+		this.wiggle_timer_ += Time.deltaTime * 0.75f;
+		while( this.wiggle_timer_ > 1.0f ) this.wiggle_timer_ -= 1.0f;
+		var smooth = Mathf.Sin(wiggle_timer_ * 2 * Mathf.PI) * weight;
+		var a = smooth * 10;
+		this.transform.localRotation = base_face * Quaternion.Euler(0, 0, a);
 	}
 }
